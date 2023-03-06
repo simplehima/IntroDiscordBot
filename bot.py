01f03f61cfb979319c20179d7248ca55b43c3846
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2023-03-06 16:05:10 +0200

    Update bot.py
    This modification checks if the bot is already connected to a voice channel in the same guild as the new one, and if it is, it either moves to the new channel or returns if it's already connected to it.

--- bot.py
+++ bot.py
@@ -18,25 +18,28 @@ async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
         channel = after.channel
 
-        # check if the bot is already connected to the voice channel
+        # check if the bot is already connected to a voice channel in this guild
         if channel.guild.id in connected_voice_channels:
             voice = connected_voice_channels[channel.guild.id]
-            if voice.channel == channel:
-                return  # already connected to the same channel
+            if voice.is_connected():  # check if the bot is already connected
+                if voice.channel == channel:
+                    return  # already connected to the same channel
+                else:
+                    await voice.move_to(channel)  # move to the new channel
+                    return
 
-            await voice.move_to(channel)
-        else:
-            try:
-                vc = await channel.connect()
-                connected_voice_channels[channel.guild.id] = vc
-                vc.play(discord.FFmpegPCMAudio('file.mp3')) #change file.mp3 with your file.mp3
-                while vc.is_playing():
-                    await asyncio.sleep(1)
-                await vc.disconnect()
-                del connected_voice_channels[channel.guild.id]
-            except asyncio.TimeoutError:
-                print("Connection to voice channel timed out.")
-            except Exception as e:
-                print(f"Failed to connect to voice channel: {e}")
+        # bot is not connected to a voice channel in this guild, connect to the new one
+        try:
+            vc = await channel.connect()
+            connected_voice_channels[channel.guild.id] = vc
+            vc.play(discord.FFmpegPCMAudio('nohooon.mp3'))
+            while vc.is_playing():
+                await asyncio.sleep(1)
+            await vc.disconnect()
+            del connected_voice_channels[channel.guild.id]
+        except asyncio.TimeoutError:
+            print("Connection to voice channel timed out.")
+        except Exception as e:
+            print(f"Failed to connect to voice channel: {e}")
 
 client.run(config.BOT_TOKEN)
