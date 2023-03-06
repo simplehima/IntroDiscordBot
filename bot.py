bde6e2098d24c114d704484b05e0e4110341b0aa
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2023-03-06 16:51:30 +0200

    added reconnection attempts Function
    added function to handle reconnection attempts

--- bot.py
+++ bot.py
@@ -1,45 +1,94 @@
-import discord
-import asyncio
-from discord.ext import commands
-from discord.voice_client import VoiceClient
-import config
+const fs = require('fs');
+const path = require('path');
+const { Client, Intents } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const ytdl = require('ytdl-core-discord');
+const config = require('./config.js');
 
-intents = discord.Intents.default()
-intents.members = True
-intents.guild_messages = True
-intents.voice_states = True
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client = commands.Bot(command_prefix='!', intents=intents)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let connectionAttempts = 0;
+let connectionTimer;
 
-connected_voice_channels = {}  # keep track of connected voice channels
+client.once('ready', async () => {
+    console.log('Ready!');
+    const rest = new REST({ version: '9' }).setToken(config.token);
 
-@client.event
-async def on_voice_state_update(member, before, after):
-    if before.channel is None and after.channel is not None:
-        channel = after.channel
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
 
-        # check if the bot is already connected to a voice channel in this guild
-        if channel.guild.id in connected_voice_channels:
-            voice = connected_voice_channels[channel.guild.id]
-            if voice.is_connected():  # check if the bot is already connected
-                if voice.channel == channel:
-                    return  # already connected to the same channel
-                else:
-                    await voice.move_to(channel)  # move to the new channel
-                    return
+    try {
+        console.log('Started refreshing application (/) commands.');
 
-        # bot is not connected to a voice channel in this guild, connect to the new one
-        try:
-            vc = await channel.connect()
-            connected_voice_channels[channel.guild.id] = vc
-            vc.play(discord.FFmpegPCMAudio('nohooon.mp3'))
-            while vc.is_playing():
-                await asyncio.sleep(1)
-            await vc.disconnect()
-            del connected_voice_channels[channel.guild.id]
-        except asyncio.TimeoutError:
-            print("Connection to voice channel timed out.")
-        except Exception as e:
-            print(f"Failed to connect to voice channel: {e}")
+        await rest.put(
+            Routes.applicationCommands(config.clientId),
+            { body: commands },
+        );
 
-client.run(config.BOT_TOKEN)
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = require(`./commands/${interaction.commandName}`);
+    try {
+        await command.execute(interaction, ytdl);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+// Define a function to attempt to connect to the voice channel
+async function connectToVoiceChannel(channel) {
+    try {
+        const connection = await channel.join();
+        console.log(`Connected to voice channel: ${channel.name}`);
+        connectionAttempts = 0; // reset the connection attempts count
+        return connection;
+    } catch (error) {
+        console.error(`Failed to connect to voice channel: ${error}`);
+        return null;
+    }
+}
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    if (!oldState.channel && newState.channel) {
+        // A user has joined a voice channel
+        const connection = await connectToVoiceChannel(newState.channel);
+
+        if (connection) {
+            // Play audio file
+            const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=dQw4w9WgXcQ'), { type: 'opus' });
+            dispatcher.on('finish', () => {
+                connection.disconnect();
+                console.log('Disconnected from voice channel.');
+            });
+        } else {
+            // If connection fails, schedule a reconnection attempt
+            connectionAttempts++;
+            const delay = Math.min(connectionAttempts * 5000, 60000); // Increase delay between attempts up to 1 minute
+            console.log(`Attempting to reconnect in ${delay / 1000} seconds...`);
+            connectionTimer = setTimeout(() => {
+                connectToVoiceChannel(newState.channel);
+            }, delay);
+        }
+    }
+});
+
+client.on('disconnect', () => {
+    // If the client loses connection to Discord, clear the connection timer
+    console.log('Disconnected from Discord. Clearing connection timer...');
+    clearTimeout(connectionTimer);
+});
+
+client.login(config.token);
